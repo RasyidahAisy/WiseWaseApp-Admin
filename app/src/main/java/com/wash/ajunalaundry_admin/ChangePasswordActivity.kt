ee52bdb658bf5abd4fb57ff188cb9afc1f340d2d
SiTelur <gantengrizky440@gmail.com>
2023-12-09 13:32:10 +0700

    Add Icon And Fix Bug In Change Password

--- app/src/main/java/com/wash/ajunalaundry_admin/ChangePasswordActivity.kt
+++ app/src/main/java/com/wash/ajunalaundry_admin/ChangePasswordActivity.kt
@@ -1,19 +1,18 @@
 package com.wash.ajunalaundry_admin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.wash.arjunalaundry_admin.R
 import com.wash.arjunalaundry_admin.databinding.ActivityChangePasswordBinding
 
 class ChangePasswordActivity : AppCompatActivity() {
     lateinit var binding: ActivityChangePasswordBinding
-    lateinit var auth:FirebaseAuth
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChangePasswordBinding.inflate(layoutInflater)
@@ -22,24 +21,29 @@ class ChangePasswordActivity : AppCompatActivity() {
 
         binding.checkBox4.setOnCheckedChangeListener { _, isChecked -> // If the checkbox is checked, show the password.
             // Otherwise, hide the password.
-            binding.editTextTextPassword2.transformationMethod = if (isChecked) null else PasswordTransformationMethod.getInstance()
-            binding.editTextPasswordSekarang.transformationMethod = if (isChecked) null else PasswordTransformationMethod.getInstance()
+            binding.editTextTextPassword2.transformationMethod =
+                if (isChecked) null else PasswordTransformationMethod.getInstance()
+            binding.editTextPasswordSekarang.transformationMethod =
+                if (isChecked) null else PasswordTransformationMethod.getInstance()
             binding.editTextTextPassword2.clearFocus()
         }
 
         binding.button5.setOnClickListener {
 
-            if (binding.editTextTextPassword2.length() < 6){
-                Toast.makeText(this, "Password Harus Lebih Dari 6 Karakter", Toast.LENGTH_SHORT).show()
+            if (binding.editTextTextPassword2.length() < 6) {
+                Toast.makeText(this, "Password Harus Lebih Dari 6 Karakter", Toast.LENGTH_SHORT)
+                    .show()
                 return@setOnClickListener
             }
 
-            if (binding.editTextPasswordSekarang.text.isEmpty() || binding.editTextEmail.text.isEmpty()){
-                Toast.makeText(this, "Anda Harus Memasukkan Email Dan Password", Toast.LENGTH_SHORT).show()
+            if (binding.editTextPasswordSekarang.text.isEmpty() || binding.editTextEmail.text.isEmpty()) {
+                Toast.makeText(this, "Anda Harus Memasukkan Email Dan Password", Toast.LENGTH_SHORT)
+                    .show()
                 return@setOnClickListener
             }
 
-            auth.signInWithEmailAndPassword(binding.editTextEmail.text.toString(),
+            auth.signInWithEmailAndPassword(
+                binding.editTextEmail.text.toString(),
                 binding.editTextPasswordSekarang.text.toString()
             ).addOnSuccessListener {
 
@@ -51,19 +55,26 @@ class ChangePasswordActivity : AppCompatActivity() {
                 user!!.updatePassword(newPassword)
                     .addOnSuccessListener {
                         Log.d("ChangePass", "Berhasil Mengganti Password")
-                        Toast.makeText(this, "Anda Berhasil Mengubah Password Anda", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this,
+                            "Anda Berhasil Mengubah Password Anda",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
                     }
                     .addOnFailureListener {
-                        Log.e("ChangePass", "onCreateView: Gagal",it)
+                        Log.e("ChangePass", "onCreateView: Gagal", it)
                     }
 
-             finish()
 
-            }.addOnFailureListener{
+            }.addOnFailureListener {
                 Toast.makeText(this, "Password Lama Anda Salah", Toast.LENGTH_SHORT).show()
 
             }
-            Toast.makeText(this, "Password Anda Salah", Toast.LENGTH_SHORT).show()
+                .addOnCanceledListener {
+                    Toast.makeText(this, "Password Anda Salah", Toast.LENGTH_SHORT).show()
+
+                }
         }
     }
 }
